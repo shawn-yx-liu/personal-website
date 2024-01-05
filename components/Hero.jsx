5f90c2cf1ab68bd6b12d1bd8f19874e65023b66d
shawn8913 <shawn.shawnliu.liu05@gmail.com>
2024-01-04 16:53:50 -0800

    renamed assets to public folder

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Hero() {
     return (
         <div className="hero">
-            <img className="hero-image" src="./assets/images/portrait_headshot.png" />
+            <img className="hero-image" src="../images/portrait_headshot.png" />
             <div className="hero-border"></div>
             <div className="hero-text">
                 <h1 className="hero-name">Hey, I'm Shawn</h1>
@@ -12,15 +12,15 @@ export default function Hero() {
                     I have been creating web applications for 2 years and am always excited to explore new frontend technologies.
                 </p>
                 <div className="hero-location">
-                    <img alt="Headshot of Shawn" className="hero-location-icon" src="../assets/icons/location.svg" />
+                    <img alt="Headshot of Shawn" className="hero-location-icon" src="../icons/location.svg" />
                     <p>Palo Alto, California</p>
                 </div>
                 <div className="hero-socials">
                     <a href="https://github.com/shawn8913" target="_blank">
-                        <img className="hero-social-icon" src="../assets/icons/github.svg" />
+                        <img className="hero-social-icon" src="../icons/github.svg" />
                     </a>
                     <a href="https://www.linkedin.com/in/shawn-liu-bbaba2158/" target="_blank">
-                        <img className="hero-social-icon" src="../assets/icons/linkedin.svg" />
+                        <img className="hero-social-icon" src="../icons/linkedin.svg" />
                     </a>
                 </div>
             </div>
